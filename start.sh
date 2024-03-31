7318d529b965eb69ecd3a32f9f18033fa7437d26
Anton Poluektov <antonp777@gmail.com>
2024-04-01 00:57:10 +0300

    API Lottery

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source /root/home/Lottery/venv/bin/activate
-screen -dmS lottery_app sudo python3 /root/home/Lottery/app/main.py
-
-source /root/home/Lottery/venv/bin/activate
-screen -dmS lottery_bot sudo python3 /root/home/Lottery/BOT/bot.py
